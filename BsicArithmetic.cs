f6f8255630d24589ba881da736bc57b4f90fe1b1
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-12-07 19:23:28 +0530

    basic math operations using Math Class

--- BsicArithmetic.cs
+++ BsicArithmetic.cs
@@ -23,6 +23,51 @@ class BsicArithmetic
 
       int remainder = friends % 3;
       Console.WriteLine("Remainder when friends is divided into 3 = "+ remainder);
+
+
+
+      //LEARING BASIC MATH CLASS;
+
+      double x = 3;
+      double a = Math.Pow(x, 2); //x squared
+      Console.WriteLine("x squared = "+ a);
+
+      Console.WriteLine("finding square root ");
+      double b = Math.Sqrt(a);
+      Console.WriteLine(b);
+
+      Console.WriteLine("let z = -3");
+      double z = -3;
+      double y = Math.Abs(z);
+      Console.WriteLine("absolute value of z is: "+ y);
+
+
+      Console.WriteLine("let x be 3.14");
+      double d = 3.14;
+      double c = Math.Round(d);
+      double f = Math.Ceiling(d);
+      double g = Math.Floor(d);
+      Console.WriteLine("x rounded is: "+ c);
+      Console.WriteLine("x ceiling (rounded up) is: "+f );
+      Console.WriteLine("x floor (rounded down) is: "+ g );
+
+
+
+
+      //finding max and min
+
+      double m = Math.Max(d, f);
+      Console.WriteLine("maximum between d and f is: "+ m);
+      
+
+      double k = Math.Min(d,g);
+      Console.WriteLine("minimum between d and g is: "+k);
+
+
+
+
+
+
     }
     
   }
