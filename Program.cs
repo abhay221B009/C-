3bda1c07792f00d574cd1125987f7b341ba8b7f5
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-12-07 17:45:25 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+    { 
+      Console.Write("\nHey!");
+      Console.WriteLine("Hello, World!");
+
+      //THIS IS A TEST COMMENT
+
+      /*THIS
+      IS 
+      A 
+      MULTI-LINE
+      COMMENT*/
+
+
+      Console.WriteLine("\tAbhay");
+      Console.ReadKey();
+    }
+    }
+} 
