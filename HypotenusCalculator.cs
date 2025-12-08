1266d7edf888171317f9bc3f2a4493d2d1de0a77
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-12-08 18:44:43 +0530

    make a basic hypotenus calculator

---
+++ HypotenusCalculator.cs
@@ -0,0 +1,22 @@
+using System;
+namespace App1
+{
+  class HypotenusCalculator
+  {
+    static void Main(string[] args)
+    {
+       Console.WriteLine("Enter side A: ");
+       double A = Convert.ToDouble(Console.ReadLine());
+
+
+       Console.WriteLine("Enter side B: ");
+       double B = Convert.ToDouble(Console.ReadLine());
+
+
+       double C = Math.Sqrt(Math.Pow(A,2)+Math.Pow(B,2));
+       double roundeC = Math.Round(C,3);
+       Console.WriteLine("The hypotenus C is: "+ roundeC);
+       
+    }
+  }
+}
