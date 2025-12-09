174f18c40defffa1f70382956c4f22822bab07f5
Abhay'sRepo <abhayrajchauhan.976@gmail.com>
2025-12-09 21:44:54 +0530

    learned about methods and return

--- Methods.cs
+++ Methods.cs
@@ -10,6 +10,20 @@ namespace App1
       singHappyBirthday(name,age);
       singHappyBirthday(name,age);
       singHappyBirthday(name,age);
+
+
+      double x , y , result;
+      Console.WriteLine("Enter first number to multiply: ");
+      
+      x= Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine("Enter second number to multiply: ");
+
+      y= Convert.ToDouble(Console.ReadLine());
+
+      result = Multiply(x,y);
+
+      Console.WriteLine("The multiplication result is: "+ result);
     }
 
     static void singHappyBirthday(string name,int age)
@@ -17,6 +31,12 @@ namespace App1
       Console.WriteLine("Happy Birthday to you!");
       Console.WriteLine("Happy Birthday to you  "+name);
       Console.WriteLine("You are "+age+" years old");
+    }
+
+    static double Multiply(double x, double y)
+    {
+      double z = x*y;
+      return z;
     }
   }
 }
--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1303f3375d191bc99929051fc392a09259a05329")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffabf2b0d644fe5ce6e560ccf5145b7d42bbbb8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
